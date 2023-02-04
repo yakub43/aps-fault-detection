5261ab3b1c8b8743a6a0a32ae328cc0f5abdb978
yakub43 <yakubakku43@gmail.com>
2023-02-04 14:15:27 +0530

    udpated the requirement.txt file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="yakub",
+    author_email="yakubakku43@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
