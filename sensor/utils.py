1446a47ce569fbe8cd84754e3993e094324eaee6
yakub43 <yakubakku43@gmail.com>
2023-02-16 20:12:31 +0530

    updated data transformation pipeline

--- sensor/utils.py
+++ sensor/utils.py
@@ -49,3 +49,28 @@ def convert_columns_float(df:pd.DataFrame,exclude_columns:list):
         return df
     except Exception as e:
         raise e
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise SensorException(e, sys) from e
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) from e
