a67ef7ff974d8f0c77a9f992b6010fe239d6b452
yakub43 <yakubakku43@gmail.com>
2023-02-16 20:34:15 +0530

    model training is completed

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -16,3 +16,19 @@ class DataTransformationArtifact:
     transformed_train_path:str
     transformed_test_path:str
     target_encoder_path:str
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted:bool
+    improved_accuracy:float
+
+@dataclass
+class ModelPusherArtifact:
+    pusher_model_dir:str 
+    saved_model_dir:str
